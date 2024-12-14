0641bc4451f11e036a7b3e107ef33f89ec0377ce
edison <845292122@qq.com>
2024-12-14 22:12:18 +0800

    新增账户信息

--- src/layout/components/AppHeader.tsx
+++ src/layout/components/AppHeader.tsx
@@ -1,8 +1,7 @@
-import { Nav, Button, Avatar, Layout } from '@douyinfe/semi-ui'
-import { IconBell, IconHelpCircle } from '@douyinfe/semi-icons'
+import { Nav, Button, Avatar, Layout, Dropdown, Typography } from '@douyinfe/semi-ui'
+import { IconBell, IconChevronUpDown, IconExit, IconHelpCircle, IconKey, IconQuit, IconUserSetting } from '@douyinfe/semi-icons'
 import styled from 'styled-components'
 import { useRouteMeta } from '~/hooks'
-import { Typography } from '@douyinfe/semi-ui'
 
 const StyledHeader = styled(Layout.Header)`
   position: sticky;
@@ -18,6 +17,19 @@ const StyleBar = styled.div`
   background-color: #1d1db7;
 `
 
+const ProfileBar = styled.div`
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  padding: 0 10px;
+  border-radius: 4px;
+  height: 40px;
+  &:hover {
+    background-color: #f5f5f5;
+  }
+`
+
+// * 标题栏
 const AppTitle = () => {
   const { title } = useRouteMeta()
   return (
@@ -28,36 +40,56 @@ const AppTitle = () => {
   )
 }
 
+// * 操作栏
+const OperationBar = () => {
+  return (
+    <>
+      <Button
+        theme="borderless"
+        icon={<IconBell size="large" />}
+        style={{
+          color: 'var(--semi-color-text-2)',
+          marginRight: '12px'
+        }}
+      />
+      <Button
+        theme="borderless"
+        icon={<IconHelpCircle size="large" />}
+        style={{
+          color: 'var(--semi-color-text-2)',
+          marginRight: '12px'
+        }}
+      />
+      <Dropdown
+        trigger="click"
+        position="bottom"
+        render={
+          <Dropdown.Menu style={{ width: '160px' }}>
+            <Dropdown.Item icon={<IconUserSetting />}>账户信息</Dropdown.Item>
+            <Dropdown.Item icon={<IconKey />}>修改密码</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item type="danger" icon={<IconExit />}>
+              注销登录
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        }
+      >
+        <ProfileBar>
+          <Avatar color="red" style={{ marginRight: '8px' }} shape="square" size="small">
+            EZ
+          </Avatar>
+          <Typography.Text strong>失败的man</Typography.Text>
+          <IconChevronUpDown style={{ marginLeft: '20px' }} />
+        </ProfileBar>
+      </Dropdown>
+    </>
+  )
+}
+
 export default function AppHeader() {
   return (
     <StyledHeader>
-      <Nav
-        mode="horizontal"
-        header={<AppTitle />}
-        footer={
-          <>
-            <Button
-              theme="borderless"
-              icon={<IconBell size="large" />}
-              style={{
-                color: 'var(--semi-color-text-2)',
-                marginRight: '12px'
-              }}
-            />
-            <Button
-              theme="borderless"
-              icon={<IconHelpCircle size="large" />}
-              style={{
-                color: 'var(--semi-color-text-2)',
-                marginRight: '12px'
-              }}
-            />
-            <Avatar color="orange" size="small">
-              YJ
-            </Avatar>
-          </>
-        }
-      ></Nav>
+      <Nav mode="horizontal" header={<AppTitle />} footer={<OperationBar />} />
     </StyledHeader>
   )
 }
